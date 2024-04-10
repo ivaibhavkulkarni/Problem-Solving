c555fb9c8570d739f2d435fceaa1e2a6b95aa5ac
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-04-10 16:52:29 +0530

    No of Anti Rotation

---
+++ Rotation_from_end.py
@@ -0,0 +1,19 @@
+def no_of_rotations(a,b):
+    if len(a) != len(b):
+        return "No Match"
+    else:
+        len_a = len(a)
+        for i in range(len_a):
+            i = i * (-1)
+            second_part = a[i:]
+            first_part = a[:i]
+            final = second_part + first_part
+            if final == b:
+                return i * (-1)
+
+a = input()
+b = input()
+
+final = no_of_rotations(a,b)
+
+print(final)
---
+++ pratice2.py
@@ -0,0 +1,5 @@
+a = "Python"
+n = int(input()) * -1
+second_part = a[n:]
+first_part = a[:n]
+print(second_part+first_part)
