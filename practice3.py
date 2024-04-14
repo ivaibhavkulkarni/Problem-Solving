602a0fb355d8594913cc26f18328270fb8ccf3ce
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-04-14 15:38:09 +0530

    First & last num

--- practice3.py
+++ practice3.py
@@ -1,15 +1,9 @@
-def converting_int(num_list):
-    num_list_int = []
-    for i in num_list:
-        i = int(i)
-        num_list_int.append(i)
-    return num_list_int
+m = int(input())
+n = int(input())
 
-num_list = input().split()
-num_list_int = converting_int(num_list)
-length = len(num_list_int)
-
-for i in range(length):
-    current_sum = 0
-    for j in range(i,length):
-        
+count = 0
+for i in range(m,n+1):
+    i = str(i)
+    if i[0] == i[-1]:
+        count += 1
+print(count)
