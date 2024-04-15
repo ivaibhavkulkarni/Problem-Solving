3b32ec19772b150b0e75739ea15ac11a96d1b3b6
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-04-15 21:36:31 +0530

    Oops Concepts

---
+++ OOP/Creating_class.py
@@ -0,0 +1,29 @@
+# Assigning values to Properties we use __int__ method
+
+class Mobile:
+    
+    # Constructor to initialize properties
+    def __init__(self, Model,Camera):
+        self.Model = Model
+        self.Camera = Camera
+
+    # acessing inside attribute
+
+    def Get_attri(self):
+        print(self.Model)
+
+
+    # Action method
+    def Making_call(self, number):
+        print("Calling...{}".format(number))
+
+# Creating an Object
+Mobile_1 = Mobile("IPhone 4s", "5 Mp")
+Mobile_1.Making_call(95050403)
+print(type(Mobile_1))
+Mobile_1.Get_attri()
+
+
+# NOTE :
+# Properties are refered as Attributes
+# Actions are reffered as Methods
